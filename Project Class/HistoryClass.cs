eae42eb54feea745b309e91f4ca5c0806f795e81
K3NTUB3 <kentuckymachenzie@gmail.com>
2024-10-16 20:31:34 +0800

    Created a class for the calculation history to be used in a User Control form. (Unfinished)

---
+++ Project Class/HistoryClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorWebApp.Project_Class
+{
+    internal class HistoryClass
+    {
+        private string history;
+        public HistoryClass(string input) 
+        {
+            history = input;
+        }
+    }
+}
