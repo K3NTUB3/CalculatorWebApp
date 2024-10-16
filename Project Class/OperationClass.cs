5d36b4a9bce7f32a388699c78b032efb0472fcc3
K3NTUB3 <kentuckymachenzie@gmail.com>
2024-10-16 20:32:48 +0800

    created a class where the user's input will be processed.

--- Project Class/OperationClass.cs
+++ Project Class/OperationClass.cs
@@ -5,6 +5,40 @@ namespace CalculatorWebApp.Project_Class
 {
     internal class OperationClass
     {
-       
+        public double ProcessExpression(string input)
+        {
+            char[] operators = new char[] { '+', '-', '*', '/' };
+            string[] numbers = input.Split(operators);
+
+            double result = double.Parse(numbers[0]);
+
+            int currentIndex = numbers[0].Length;
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                char currentOperator = input[currentIndex];
+                double nextNumber = double.Parse(numbers[i]); 
+
+                switch (currentOperator)
+                {
+                    case '+':
+                        result += nextNumber;
+                        break;
+                    case '-':
+                        result -= nextNumber;
+                        break;
+                    case '*':
+                        result *= nextNumber;
+                        break;
+                    case '/':
+                        result /= nextNumber;
+                        break;
+                }
+
+                currentIndex += numbers[i].Length + 1;
+            }
+
+            return result;
+        }
     }
 }
